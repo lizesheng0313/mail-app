ea08f80bfa750230ff650d005e6e6d7fcae5d705
lizesheng <lizesheng0313@gmail.com>
2026-04-28 13:44:46 +0800

    feat: 调整下载参数

--- scripts/update_desktop_release.js
+++ scripts/update_desktop_release.js
@@ -22,7 +22,7 @@ const DESKTOP_RELEASE_BASE_URL = 'https://zjkdongao.cn/downloads'
 
 export const DESKTOP_DOWNLOAD_URLS = {
   macos: \`\${DESKTOP_RELEASE_BASE_URL}/mail-desktop_\${DESKTOP_RELEASE_VERSION}_universal.dmg\`,
-  windows: \`\${DESKTOP_RELEASE_BASE_URL}/mail-desktop_\${DESKTOP_RELEASE_VERSION}_x64-setup.exe\`,
+  windows: \`\${DESKTOP_RELEASE_BASE_URL}/mail-desktop_\${DESKTOP_RELEASE_VERSION}_x64-setup.nsis.zip\`,
 }
 `;
 
